fac501ef06b1ee0f82601b9f59a02e56d138e5f0
Yogi-Vishwas <107536993+Yogi-Vishwas@users.noreply.github.com>
2023-09-11 16:13:08 +0530

    fix(core): ARC-62 add cross platform config generator (#65)
    * fix(core): ARC-62 add cross platform config generator
    
    * chore: ARC-62 fix vulnerabilities
    
    ---------
    
    Co-authored-by: Shubham P <shubham.prajapat@sourcefuse.com>

--- Dockerfile
+++ Dockerfile
@@ -6,23 +6,16 @@ RUN npm install --omit=dev
 COPY . .
 RUN npm run build
 
+# Generate the app config using configGenerator.js
+RUN node configGenerator.js
+
 # production environment
 FROM nginx:stable-alpine
 COPY --from=build /app/build /usr/share/nginx/html
 COPY --from=build /app/nginx/nginx.conf /etc/nginx/conf.d/default.conf
 COPY --from=build /app/nginx/gzip.conf /etc/nginx/conf.d/gzip.conf
+COPY --from=build /app/config.json /usr/share/nginx/html
 EXPOSE 80
 
-# Copy .env file and shell script to container
-WORKDIR /usr/share/nginx/html
-COPY ./env.sh .
-COPY ./config.template.json .
-
-# Add bash
-RUN apk add --no-cache bash
-
-# Make our shell script executable
-RUN chmod +x env.sh
-
 # Start Nginx server
-CMD ["/bin/bash", "-c", "/usr/share/nginx/html/env.sh && nginx -g \"daemon off;\""]
+CMD ["nginx", "-g", "daemon off;"]
---
+++ configGenerator.js
@@ -0,0 +1,73 @@
+/* eslint-env node */
+import fs from 'fs';
+import path, {dirname} from 'path';
+import yargs from 'yargs';
+import dotenv from 'dotenv';
+import {fileURLToPath} from 'url';
+
+// Get the directory name and filename
+const currentDir = dirname(fileURLToPath(import.meta.url));
+const envFilePath = path.join(currentDir, '.env');
+
+try {
+  // Read the environment variables from .env file
+  const envFile = fs.readFileSync(envFilePath);
+  const envVariables = dotenv.parse(envFile);
+
+  // Ensure required environment variables are present
+  const requiredEnvVars = ['CLIENT_ID', 'APP_API_BASE_URL']; // Load from a config file for flexibility
+  for (const varName of requiredEnvVars) {
+    if (!(varName in envVariables)) {
+      handleError(`Required environment variable "${varName}" not found.`);
+    }
+  }
+
+  // Read the command line arguments
+  const argv = yargs(process.argv).argv;
+  const {templateFileName, outConfigPath, configFileName = 'config.json'} = argv;
+
+  // Validate and construct file paths
+  if (!templateFileName || !outConfigPath) {
+    handleError(`Missing required command line arguments.`);
+  }
+
+  const templateFilePath = path.join(currentDir, templateFileName);
+  const outConfigFilePath = path.join(outConfigPath, configFileName);
+  const currentConfigFilePath = path.join(currentDir, configFileName);
+
+  // Check if template file exists
+  if (!fs.existsSync(templateFilePath)) {
+    handleError(`Template file "${templateFileName}" does not exist.`);
+  }
+
+  // Read the template file
+  const templateData = fs.readFileSync(templateFilePath, 'utf-8');
+
+  // Replace the placeholders with environment variables
+  const substitutedData = templateData.replace(/\$(\w+)/g, (_, key) => {
+    return envVariables[key] || '';
+  });
+
+  // Write the substituted data to config.json
+  try {
+    fs.writeFileSync(currentConfigFilePath, substitutedData, 'utf-8');
+    console.log('Configuration file written in current directory.');
+
+    // Copy config.json to the public directory
+    try {
+      fs.copyFileSync(currentConfigFilePath, outConfigFilePath);
+      console.log('Configuration file copied to public directory.');
+    } catch (copyError) {
+      handleError(`Error copying config file: ${copyError.message}`);
+    }
+  } catch (writeError) {
+    handleError(`Error writing config file: ${writeError.message}`);
+  }
+} catch (error) {
+  handleError(`An error occurred: ${error.message}`);
+}
+
+function handleError(message) {
+  console.error(message);
+  process.exit(1);
+}
